7c9f7572e8ba43ef72ebea39ca3af64d59cf6e5f
Jarvis Bryant <98724740+JarvisB92@users.noreply.github.com>
2022-04-17 00:26:59 -0500

    Create AWS Cloud 9_Boto3Practice.py

---
+++ AWS Cloud 9_Boto3Practice.py
@@ -0,0 +1,21 @@
+""" 
+Your module description
+"""
+
+import random
+import boto3 
+
+age = 25
+stringy = "2f5"
+light_bulb = True
+
+random_number_1 = random.randint(0,50)
+#Python Practice
+
+print(random_number_1)
+print(light_bulb)
+print(age)
+print(stringy)
+#Practicing running variables for desired output
+
+
