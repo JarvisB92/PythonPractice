045c9dd3c1e3ad7726ae89526918f8f7352e79d4
Jarvis Bryant <98724740+JarvisB92@users.noreply.github.com>
2022-04-18 23:31:16 -0500

    Create UserWelcome_Message.py

---
+++ UserWelcome_Message.py
@@ -0,0 +1,16 @@
+"""
+Your module description
+"""
+#Create a welcome message for a user
+
+#First Name
+user_FN = ("Hi, What is your first name?")
+
+#Last Name
+user_LN = ("Could you provide your last name?")
+
+#Greeting Message
+user_greet = ("Welcome to the World of GitHub with Jarvis Bryant LU!!")
+
+print(user_FN, user_LN) 
+print(user_greet)
